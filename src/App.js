87fa6192763e518a62338eb714feda77997bd541
Pro7Prabhakar <prabhashankarmishra202@gmail.com>
2024-04-24 12:26:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import * from './components/1';
 import Axios from 'axios';
 import { useState } from 'react';
+import pokemon from './pokemon.json';
 
 function App() {
   const [joke, SetJoke] = useState("");
@@ -18,10 +19,48 @@ function App() {
   }
   return (
     <>
-    <div style={{textAlign: 'center'}}>Hllo
+    <div style={{textAlign: 'center'}}>Hello
     <br/>
     <button onClick={getJoke}>Click me for jokes</button><br/><hr/>
-    {joke}
+    {joke}<br/>
+    </div>
+    <div
+      style={
+        {margin: 'auto',
+        width : 800,
+        paddingTop: '1rem',
+      }
+      } 
+    >
+      <h1 className='title'>Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Type
+          </th>
+        </tr>
+        </thead>
+        
+        <tbody>
+          {pokemon.slice(0, 20).map(pokemon => (
+            // <tr key={pokemon.id}>
+            <tr key={[pokemon.name.english].join(':')}
+            >
+            <td>
+              {pokemon.name.english}
+            </td>
+            <td>
+              {pokemon.type.join(', ')}
+            </td>
+            </tr>
+          ))}
+          
+        </tbody>
+      </table>
     </div>
     </>
   );
